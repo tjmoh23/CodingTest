6957287fa653cbd3665f472ce7e156e6b5456923
이호연 <51068987+tjmoh23@users.noreply.github.com>
2025-11-19 19:37:58 +0900

    Update ［파이썬 프로그래밍 기초（1） 파이썬의 기본 구조와 기초 문법］ 8． 함수의 기초 8.py

--- SWEA/D1/6327. ［파이썬 프로그래밍 기초（1） 파이썬의 기본 구조와 기초 문법］ 8． 함수의 기초 8/［파이썬 프로그래밍 기초（1） 파이썬의 기본 구조와 기초 문법］ 8． 함수의 기초 8.py
+++ SWEA/D1/6327. ［파이썬 프로그래밍 기초（1） 파이썬의 기본 구조와 기초 문법］ 8． 함수의 기초 8/［파이썬 프로그래밍 기초（1） 파이썬의 기본 구조와 기초 문법］ 8． 함수의 기초 8.py
@@ -7,3 +7,20 @@ nums = input().split(',')
 for num in nums:
     n = int(num.strip())     # 공백 제거 + 정수 변환
     print(f"square({n}) => {square(n)}")
+
+
+
+'''
+# 이건 끼워 맞ㅊ기
+a, b = map(int, input().split(','))
+ 
+def square(a, b):
+    sq_a = a**2
+    sq_b = b**2
+    print(f'square({a}) => {sq_a}')
+    print(f'square({b}) => {sq_b}')    
+    return
+ 
+square(a, b)
+
+'''
